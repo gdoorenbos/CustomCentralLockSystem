5bdaa5dea8e64c780bbc77665baa374a59414aa2
jetpacksWasYes <gdoorenbos@yahoo.com>
2016-05-29 23:26:13 -0500

    made cls global
    so that run() can be called in loop()

--- CclsBluetoothF150/CclsBluetoothF150_Uno/CclsBluetoothF150_Uno.ino
+++ CclsBluetoothF150/CclsBluetoothF150_Uno/CclsBluetoothF150_Uno.ino
@@ -1,17 +1,20 @@
 #include "DriverFactoryImpl.h"
 #include <BluetoothCentralLockSystem.h>
 
+BluetoothCentralLockSystem* cls;
+
 void setup()
 {
     DriverFactory* df = DriverFactoryImpl::getInstance();
-    BluetoothCentralLockSystem cls( df->getBluetoothDriver()
-                                  , df->getPowerLocksDriver()
-                                  , df->getLockButton()
-                                  , df->getUnlockButton() );
+    cls = new BluetoothCentralLockSystem( df->getBluetoothDriver()
+                                        , df->getPowerLocksDriver()
+                                        , df->getLockButton()
+                                        , df->getUnlockButton() );
 }
 
 void loop()
 {
-    
+    cls->run();
+    delay(100);
 }
 
