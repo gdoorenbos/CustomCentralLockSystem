3aab8397110bd89274f9da05ff05f2e1d2c89519
Garrett <gdoorenbos@yahoo.com>
2016-06-07 07:19:03 -0500

    Adding BluetoothCentralLockSystem to unit test build system

--- tests/source/BluetoothCentralLockSystemTest.cpp
+++ tests/source/BluetoothCentralLockSystemTest.cpp
@@ -1,7 +1,16 @@
 #include "gtest/gtest.h"
+#include "MockBluetoothDriver.h"
+#include "MockPowerLocksDriver.h"
+#include "MockPushButtonDriver.h"
+// #include "BluetoothCentralLockSystem.h"
 
 // todo: replace with real tests
 TEST(BluetoothCentralLockSystemTest, dummy)
 {
+	MockBluetoothDriver bluetooth;
+	MockPowerLocksDriver locks;
+	MockPushButtonDriver lockButton;
+	MockPushButtonDriver unlockButton;
+	// BluetoothCentralLockSystem cls(&bluetooth, &locks, &lockButton, &unlockButton);
 	EXPECT_TRUE(true);
 }
