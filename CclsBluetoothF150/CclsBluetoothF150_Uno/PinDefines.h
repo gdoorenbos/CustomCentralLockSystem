f5826d13e5b6febafa1669dd14ce5e4cde7fb4be
jetpacksWasYes <gdoorenbos@yahoo.com>
2016-06-26 22:38:48 -0500

    Housekeeping
    * Removed Attiny cores - could not get program to run on attiny84.
    Program does not fit on attiny2313.
    * Added cores for atmega328 on breadboard using 8Mhz internal clock
    * rearranged .gitignore and PinDefines.h to be more organized.

--- CclsBluetoothF150/CclsBluetoothF150_Uno/PinDefines.h
+++ CclsBluetoothF150/CclsBluetoothF150_Uno/PinDefines.h
@@ -1,16 +1,16 @@
 #ifndef PIN_DEFINES_H
 #define PIN_DEFINES_H
 
-// Push buttons
-#define PUSH_BUTTON_LOCK_PIN    6
-#define PUSH_BUTTON_UNLOCK_PIN  7
-
 // Bluetooth
-#define BLUETOOTH_SERIAL_RX_PIN 3
 #define BLUETOOTH_SERIAL_TX_PIN 2
+#define BLUETOOTH_SERIAL_RX_PIN 3
 
 // Power Locks
 #define POWER_LOCKS_LOCK_PIN    4
 #define POWER_LOCKS_UNLOCK_PIN  5
+
+// Push buttons
+#define PUSH_BUTTON_LOCK_PIN    6
+#define PUSH_BUTTON_UNLOCK_PIN  7
 
 #endif
