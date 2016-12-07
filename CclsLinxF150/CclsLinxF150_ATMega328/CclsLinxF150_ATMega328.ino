44e44dcccb32099499c598657f42fe3465ea57a1
jetpacksWasYes <gdoorenbos@yahoo.com>
2016-12-06 20:48:23 -0600

    Fixing build errors in .ino file

--- CclsLinxF150/CclsLinxF150_ATMega328/CclsLinxF150_ATMega328.ino
+++ CclsLinxF150/CclsLinxF150_ATMega328/CclsLinxF150_ATMega328.ino
@@ -1,4 +1,5 @@
 #include <LinxCentralLockSystem.h>
+#include <LinxRxModule.h>
 #include "DriverFactoryImpl.h"
 #include "LinxDeviceAddress.h"
 
@@ -7,7 +8,7 @@ LinxCentralLockSystem* cls;
 void setup()
 {
     DriverFactory* df = DriverFactoryImpl::getInstance();
-    df->getLinuxRxModule()->setAddress(LINX_DEVICE_ADDRESS);
+    df->getLinxRxModule()->setAddress(LINX_DEVICE_ADDRESS);
     cls = new LinxCentralLockSystem( df->getLinxRxModule()
                                    , df->getPowerLocksDriver() );
 }
